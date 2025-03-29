e95fb5354c27c1ee05bae9d7a91d3a16dbb2a0e7
FuadAbdullah97 <ifuadabdullah@gmail.com>
2025-03-29 23:24:45 +0600

    Front-end Complete

--- gimini-clone/src/components/Main/Main.jsx
+++ gimini-clone/src/components/Main/Main.jsx
@@ -30,6 +30,10 @@ function Main(props) {
                         <p>Briefly summarize this concept : urban Planing</p>
                         <img src={assets.compass_icon} alt=""/>
                     </div>
+                    <div className="card">
+                        <p>Briefly summarize this concept : urban Planing</p>
+                        <img src={assets.compass_icon} alt=""/>
+                    </div>
                     <div className="main-bottom">
                         <div className="search-box">
                             <input type="text" placeholder='Enter a prompt here...'/>
